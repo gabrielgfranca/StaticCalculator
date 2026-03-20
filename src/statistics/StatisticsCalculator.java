d8492abeafd6a11cb656f5d2fb8e578c735dc2eb
Gabriel França <gabrielgfranca7@proton.me>
2026-03-20 17:55:47 -0300

    feat: sum, arithmetic and geometric mean

--- src/statistics/StatisticsCalculator.java
+++ src/statistics/StatisticsCalculator.java
@@ -3,13 +3,37 @@ package statistics;
 import java.util.List;
 
 public class StatisticsCalculator {
-    
-    public double mean(List<Integer> values) {
-        return 1;
+
+    public double sum(List<Integer> values){
+        double sum = 0;
+
+        for (int value : values) {
+            sum += values.get(value);
+        }
+
+        return sum;
+    }
+
+    public double arithmeticMean(List<Integer> values) {
+        if (values.isEmpty()) {
+            throw new IllegalArgumentException("The list cannot be empty.");
+        }
+
+        return sum(values) / values.size();
     }
 
     public double geometricMean(List<Integer> values) {
-        return 1;
+        if (values.isEmpty())
+            throw new IllegalArgumentException("The list cannot be empty.");
+        if (values.size() <= 0)
+            throw new IllegalArgumentException("Values must be positive for geometric mean.");
+
+        double product = 1.0;
+        for (int value : values) {
+            product *= value;
+        }
+
+        return Math.pow(product, 1.0 / values.size());
     }
 
     public double variance(List<Integer> values) {
