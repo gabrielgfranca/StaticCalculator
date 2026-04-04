136025a11b8f6e69344c5c1290e96dd1138a4292
Gabriel França <gabrielgfranca7@proton.me>
2026-04-03 23:46:49 -0300

    feat: variance and List type changed to Double

--- DEVELOPMENT_LOG.md
+++ DEVELOPMENT_LOG.md
@@ -1,8 +1,19 @@
 # Development Log
 
-## Day 1 - 03/12/2026
+## Day 1
 Project setup.
+
 - [x] Created repository
 - [x] Defined initial architecture
 - [x] Created project packages
+
+## Day 2
+Development of the Statistical Calculator function
+
+- [x] Change List type to Double
+- [x] sum
+- [x] arithmeticMean
+- [x] sumgeometricMean
+- [ ] standardDeviation
+- [ ] range
 
--- src/statistics/StatisticsCalculator.java
+++ src/statistics/StatisticsCalculator.java
@@ -4,47 +4,59 @@ import java.util.List;
 
 public class StatisticsCalculator {
 
-    public double sum(List<Integer> values){
+    public double sum(List<Double> values){
+        
         double sum = 0;
 
-        for (int value : values) {
-            sum += values.get(value);
-        }
+        for (double i : values)
+            sum += values.indexOf(i);
 
         return sum;
     }
 
-    public double arithmeticMean(List<Integer> values) {
-        if (values.isEmpty()) {
+    public double arithmeticMean(List<Double> values) {
+        
+        if (values.isEmpty())
             throw new IllegalArgumentException("The list cannot be empty.");
-        }
 
         return sum(values) / values.size();
     }
 
-    public double geometricMean(List<Integer> values) {
+    public double geometricMean(List<Double> values) {
+        
         if (values.isEmpty())
             throw new IllegalArgumentException("The list cannot be empty.");
+
         if (values.size() <= 0)
             throw new IllegalArgumentException("Values must be positive for geometric mean.");
 
         double product = 1.0;
-        for (int value : values) {
-            product *= value;
-        }
+        for (double i : values)
+            product *= i;
 
         return Math.pow(product, 1.0 / values.size());
     }
 
-    public double variance(List<Integer> values) {
+    public double variance(List<Double> values) {
+        
+        double mean = arithmeticMean(values);
+        
+        double variance  = 0;
+
+        for (double i : values) {
+            variance += (i - mean) * (i - mean);
+        }
+        
+        variance /= (values.size() - 1);
+
+        return variance;
+    }
+
+    public double standardDeviation(List<Double> values) {
         return 1;
     }
 
-    public double standardDeviation(List<Integer> values) {
-        return 1;
-    }
-
-    public int range(List<Integer> values) {
+    public int range(List<Double> values) {
         return 1;
     }
 }
